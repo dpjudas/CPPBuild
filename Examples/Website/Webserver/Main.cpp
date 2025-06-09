92c5e5a3c4ab90708ea21a48172a68b46901f8b3
Magnus Norddahl <dpjudas@users.noreply.github.com>
2025-06-09 08:54:06 +0200

    Create a more complex website example that builds a webserver and a small webassembly single page application

--- Examples/Website/Webserver/Main.cpp
+++ Examples/Website/Webserver/Main.cpp
@@ -1,9 +1,24 @@
 
+#include "Precomp.h"
 #include "Main.h"
+#include "Webserver/ActionModule.h"
+#include "Webserver/FileModule.h"
+#include "Webserver/Webserver.h"
+#include "IOData/Directory.h"
+#include "IOData/File.h"
+#include "IOData/FilePath.h"
 #include <iostream>
 
 int main()
 {
-	std::cout << "Hello World!" << std::endl;
+	auto logger = ConsoleLogger::create();
+
+	auto webserver = Webserver::create(WebserverType::tcpSocket);
+	webserver->addModule(FileModule::createZip("/", FilePath::combine(Directory::exePath(), "Website.webpkg"), "Website.html"));
+	webserver->start();
+
+	char c;
+	std::cin >> c;
+
 	return 0;
 }
--- Examples/Website/Website/Main.cpp
+++ Examples/Website/Website/Main.cpp
@@ -1,9 +1,23 @@
 
 #include "Main.h"
 #include <iostream>
+#include <string>
+#include <emscripten/val.h>
+
+using JSValue = emscripten::val;
 
 int main()
 {
 	std::cout << "Hello World!" << std::endl;
+
+	std::string imgTag = "img";
+	std::string attrName = "src";
+	std::string src = "reticulated-giraffe.webp";
+
+	JSValue body = JSValue::global("document")["body"];
+	JSValue img = JSValue::global("document").call<JSValue>("createElement", imgTag);
+	img.call<void>("setAttribute", attrName, src);
+	body.call<void>("appendChild", img);
+
 	return 0;
 }

50059ce89ff7938fb06054621ce86a2aa3afc849
dpjudas <dpjudas@users.noreply.github.com>
2025-06-22 04:06:37 +0200

    Add installers to javascript

--- CPPBuild/CPPBuildJS.h
+++ CPPBuild/CPPBuildJS.h
@@ -3,7 +3,7 @@
 static const char* cppbuildJS = R"xxxx(
 
 import { native } from "cppbuild/native";
-export { Project, Targets, File, FilePath, Directory, Environment };
+export { Project, Targets, Installers, File, FilePath, Directory, Environment };
 
 class Target
 {
@@ -125,6 +125,7 @@ class Configuration
 var projectName = "";
 var configList = [];
 var targetList = [];
+var installerList = [];
 
 class Targets
 {
@@ -308,11 +309,146 @@ class Environment
 	}
 }
 
+class Installer
+{
+	constructor(subdirectory, name) {
+		this.subdirectory = subdirectory;
+		this.name = name;
+		this.components = [];
+		this.features = [];
+		this.msiProductName = name;
+		this.msiProductVersion = "01.01.0000";
+		this.msiManufacturer = "";
+		this.msiProductCode = "";
+		this.msiUpgradeCode = "";
+		this.msiPackageCode = "";
+		this.msiProductKeywords = [ "Installer" ];
+	}
+
+	setMsiProductName(name) {
+		this.msiProductName = name;
+	}
+
+	setMsiProductVersion(version) {
+		this.msiProductVersion = version;
+	}
+
+	setMsiManufacturer(manufacturer) {
+		this.msiManufacturer = manufacturer;
+	}
+
+	setMsiProductCode(productCode) {
+		this.msiProductCode = productCode;
+	}
+
+	setMsiUpgradeCode(upgradeCode) {
+		this.msiUpgradeCode = upgradeCode;
+	}
+
+	setMsiPackageCode(packageCode) {
+		this.msiPackageCode = packageCode;
+	}
+
+	addMsiProductKeywords(keywords) {
+		var self = this;
+		keywords.forEach(function(keyword) { self.addMsiProductKeyword(keyword); });
+	}
+
+	addMsiProductKeyword(keyword) {
+		this.msiProductKeywords.push(keyword);
+	}
+
+	addComponent(name) {
+		var component = new InstallerComponent(name);
+		this.components.push(component);
+		return component;
+	}
+
+	addFeature(name) {
+		var feature = new InstallerFeature(name);
+		this.features.push(feature);
+		return feature;
+	}
+
+	toInstallerDefinition() {
+		return {
+			subdirectory: this.subdirectory,
+			name: this.name,
+			components: this.components.map(function(component) { return component.toInstallerDefinition(); }),
+			features: this.features.map(function(feature) { return feature.toInstallerDefinition(); }),
+			msiProductName: this.msiProductName,
+			msiProductVersion: this.msiProductVersion,
+			msiManufacturer: this.msiManufacturer,
+			msiProductCode: this.msiProductCode,
+			msiUpgradeCode: this.msiUpgradeCode,
+			msiPackageCode: this.msiPackageCode,
+			msiProductKeywords: this.msiProductKeywords
+		};
+	}
+}
+
+class InstallerComponent
+{
+	constructor(name) {
+		this.name = name;
+		this.files = [];
+	}
+
+	addFiles(files) {
+		var self = this;
+		files.forEach(function(file) { self.addFile(file); });
+	}
+
+	addFile(file) {
+		this.files.push(file);
+	}
+
+	toInstallerDefinition() {
+		return {
+			name: this.name,
+			files: this.files
+		};
+	}
+}
+
+class InstallerFeature
+{
+	constructor(name) {
+		this.name = name;
+		this.components = [];
+	}
+
+	addComponents(components) {
+		var self = this;
+		components.forEach(function(component) { self.addComponent(component); });
+	}
+
+	addComponent(component) {
+		this.components.push(component);
+	}
+
+	toInstallerDefinition() {
+		return {
+			components: this.components.map(function(component) { return component.name; })
+		};
+	}
+}
+
+class Installers
+{
+	static addInstaller(name) {
+		var installer = new Installer(native.subdirectory, name);
+		installerList.push(installer);
+		return installer;
+	}
+}
+
 native.generate = function() {
 	return {
 		name: projectName,
 		targets: targetList.map(function(target) { return target.toTargetDefinition(); }),
-		configurations: configList.map(function(config) { return config.toConfigDefinition(); })
+		configurations: configList.map(function(config) { return config.toConfigDefinition(); }),
+		installers: installerList.map(function(installer) { return installer.toInstallerDefinition(); })
 	};
 };
 

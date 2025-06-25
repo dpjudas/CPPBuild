c4bf22fc0b47298b68a4ed75940558b7c232b1aa
Magnus Norddahl <dpjudas@users.noreply.github.com>
2025-06-25 17:46:04 +0200

    Implement javascript version of the FilePath class

--- CPPBuild/CPPBuildJS.h
+++ CPPBuild/CPPBuildJS.h
@@ -214,35 +214,118 @@ class File
 
 class FilePath
 {
-	static hasExtension(filename, extension) {
-		return false;
+	static hasExtension(filename, ext) {
+		return FilePath.extension(filename).toLowerCase() == ext;
 	}
 
 	static extension(filename) {
+		var file = FilePath.lastComponent(filename);
+		var pos = file.lastIndexOf(".");
+		if (pos == -1) {
+			return "";
+		}
+		else {
+			return filename.substring(pos + 1);
+		}
+	
 	}
 
 	static removeExtension(filename) {
+		var file = FilePath.lastComponent(filename);
+		var pos = file.lastIndexOf(".");
+		if (pos == -1) {
+			return filename;
+		}
+		else {
+			return filename.substring(0, filename.length - file.length + pos);
+		}
 	}
 
 	static firstComponent(path) {
+		var firstSlash = path.indexOf("/");
+		var firstBackslash = path.indexOf("\\");
+		if (firstBackslash != -1 && (firstSlash == -1 || firstSlash > firstBackslash)) {
+			firstSlash = firstBackslash;
+		}
+
+		if (firstSlash != -1) {
+			return path.substring(0, firstSlash);
+		}
+		else {
+			return path;
+		}
 	}
 
 	static removeFirstComponent(path) {
+		var firstSlash = path.indexOf("/");
+		var firstBackslash = path.indexOf("\\");
+		if (firstBackslash != -1 && (firstSlash == -1 || firstSlash > firstBackslash)) {
+			firstSlash = firstBackslash;
+		}
+
+		if (firstSlash != -1) {
+			return path.substring(firstSlash + 1);
+		}
+		else {
+			return "";
+		}
 	}
 
 	static lastComponent(path) {
+		var lastSlash = path.lastIndexOf("/");
+		var lastBackslash = path.lastIndexOf("\\");
+		if (lastSlash != -1 && lastSlash >= lastBackslash) {
+			return path.substring(lastSlash + 1);
+		}
+		else if (lastBackslash != -1 && lastBackslash >= lastSlash) {
+			return path.substring(lastBackslash + 1);
+		}
+		else {
+			return path;
+		}
 	}
 
 	static removeLastComponent(path) {
+		var lastSlash = path.lastIndexOf("/");
+		var lastBackslash = path.lastIndexOf("\\");
+		if (lastSlash != -1 && lastSlash >= lastBackslash) {
+			return path.substring(0, lastSlash);
+		}
+		else if (lastBackslash != -1 && lastBackslash >= lastSlash) {
+			return path.substring(0, lastBackslash);
+		}
+		else {
+			return "";
+		}
 	}
 
 	static combine(path1, path2) {
+		if (path1 == "") {
+			return FilePath.forceSlash(path1);
+		}
+		else if (path2 == "") {
+			return FilePath.forceSlash(path1);
+		}
+		else if (path2.startsWith("\\") || path2.startsWith("/")) {
+			return FilePath.forceSlash(path2);
+		}
+		else if (path2.length >= 2 && path2[1] == ':') {
+			return FilePath.forceSlash(path2);
+		}
+		else if (!path2.endsWith("\\") && !path2.endsWith("/")) {
+			return FilePath.forceSlash(path1 + "/" + path2);
+		}
+		else {
+			return FilePath.forceSlash(path1 + path2);
+		}
 	}
 
 	static forceSlash(path) {
+		return path.replaceAll("\\", "/");
 	}
 
 	static forceBackslash(path) {
+		return path.replaceAll("/", "\\");
 	}
 }
 
